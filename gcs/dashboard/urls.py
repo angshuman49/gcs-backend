ccedd778b296df2bbdf7033d407e01565c9e5ac1
angshuman49 <ang5human49ok+67@gmail.com>
2026-06-15 17:52:55 +0600

    Update urls.py

--- gcs/dashboard/urls.py
+++ gcs/dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import dashboard_content, dashboard_change
+from .views import dashboard_content, dashboard_change, check
 
 urlpatterns = [
     path('content/', dashboard_content, name='dashboard-content'),
     path('change-dashboard/', dashboard_change, name='dashboard-change'),
+    path('/check/', check, name='check')
 ]
